8fb59e0875543291b1a47030bf870f4e4ea26046
Sinujami <jaunius.bruzas11@gmail.com>
2024-02-15 20:35:25 +0200

    hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
-def factoriak(n):
-	if n==0:
-		return 1
-	else:
-		return n*factiorial(n-1)
-		
-number =5
-result == factorial(number)
-print(f"factorial of {number} is {result}.")
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
+number = 5
+result = factorial(number)
+print(f"Factorial of {number} is {result}.")
